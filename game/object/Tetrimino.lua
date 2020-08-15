9c98f38a9f8f25cd4cc20bfb0ea58cf624b712f3
Joseph R Miles <joe@jrm.dev>
2020-08-15 13:09:07 -0700

    Added draw functions for all types of Tetrimino

--- game/object/Tetrimino.lua
+++ game/object/Tetrimino.lua
@@ -4,7 +4,7 @@ local Tetrimino = GameObject:new
 {
     row = 0,
     column = 0,
-    type = "line"
+    type = "cyan"
 }
 
 function Tetrimino:new(column, row, type)
@@ -34,9 +34,43 @@ function Tetrimino:draw(game)
 
     local actions =
     {
-        ["line"] = 
+        ["cyan"] =
+        -- A straight line
         function (column, row)
             fillCell(column, row)
+            fillCell(column, row-1)
+            fillCell(column, row-2)
+            fillCell(column, row-3)
+        end,
+        ["blue"] =
+        -- Left-handed l-shape
+        function (column, row)
+            return
+        end,
+        ["orange"] =
+        -- Right-handed l-shape
+        function (column, row)
+            return
+        end,
+        ["yellow"] =
+        -- Square
+        function (column, row)
+            return
+        end,
+        ["green"] =
+        -- Right-handed s-shape
+        function (column, row)
+            return
+        end,
+        ["red"] =
+        -- Left-handed s-shape
+        function (column, row)
+            return
+        end,
+        ["violet"] =
+        -- T-shape
+        function (column, row)
+            return
         end,
     }
 
